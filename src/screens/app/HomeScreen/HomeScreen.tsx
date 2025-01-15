67c3c98bc4f332b39a97e58df8c3c0f1ad627502
hamilton <hamilton-souza@outlook.com.br>
2025-01-15 19:49:55 -0300

    feat(homeScreen): criado homeScreen basica

---
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "../../../components/Text/Text";
+
+export function HomeScreen() {
+  return (
+    <View>
+      <Text preset="headingLarge">HomeScreen</Text>
+    </View>
+  );
+}
